a666d0dd9711da9df72f21ba0531b7075ad114f2
99Page <nou0ggid@gmail.com>
2023-07-17 09:00:39 +0900

    멤버와이즈 이니셜라이저 public

--- Sources/cookcode-service/Domain/descendant/Recipe/DTO/RecipeFormDTO.swift
+++ Sources/cookcode-service/Domain/descendant/Recipe/DTO/RecipeFormDTO.swift
@@ -8,24 +8,19 @@
 import Foundation
 
 public struct RecipeFormDTO: Encodable {
+    public init(title: String, description: String, thumbnail: String, ingredients: [Int], optionalIngredients: [Int], steps: [StepFormDTO], isPremium: Bool? = nil) {
+        self.title = title
+        self.description = description
+        self.thumbnail = thumbnail
+        self.ingredients = ingredients
+        self.optionalIngredients = optionalIngredients
+        self.steps = steps
+        self.isPremium = isPremium
+    }
+    
     public let title, description, thumbnail: String
     public let ingredients, optionalIngredients: [Int]
     public let deletedThumbnails: [String] = []
     public var steps: [StepFormDTO]
     public var isPremium: Bool?
-    
-//    init (recipeForm: RecipeForm) {
-//        title = recipeForm.title
-//        description = recipeForm.description
-//        thumbnail = recipeForm.thumbnail
-//
-//        ingredients = recipeForm.ingredients
-//        optionalIngredients = recipeForm.optionalIngredients
-//        isPremium = recipeForm.presentToOnlyMembership
-//
-//        steps = []
-//        for i in recipeForm.steps.indices {
-//            steps.append(StepFormDTO(form: recipeForm.steps[i], seq: i+1))
-//        }
-//    }
 }
